3bea3104a08f138682c1fba05b2668bcf1e27fd7
Joel Wamser <joel.t.wamser@gmail.com>
2023-04-11 17:35:46 -0400

    feat(service): Initial implementation of server behavior added to project
    Created a Point class representing a point on the coordinate plane , as well as a Cosine class that holds state information like Amplitude, Frequency, my Point class etc, and these are used to represent the Implanted Devices state that the client app will eventually change with gRPC calls.  Also created and implemented the server-side logic for requests and responses.

---
+++ ImplantedDeviceService/ImplantedDeviceService/Services/MyCosine.cs
@@ -0,0 +1,70 @@
+﻿namespace ImplantedDeviceService.Services
+{
+    //I can probably extract functionality from this class into an interface
+    //and make it more generic for extensibility
+    public class MyCosine
+    {
+        public MyCosine() { }
+        private double coefficient_a;
+        private double coefficient_b;
+        private double coefficient_c;
+        private double coefficient_d;
+        public double SampleRate { get; set; }
+        public int SampleCount { get; set; }
+        public double Amplitude
+        {
+            get
+            {
+                return coefficient_a;
+            }
+            set => coefficient_a = value;
+        }
+        public double PhaseShift
+        {
+            get
+            {
+                return -1 * coefficient_c;
+            }
+            set
+            {
+                coefficient_c = value * -1;
+            }
+        }
+        public double Frequency
+        {
+            get
+            {
+                return (coefficient_b / 2 * Math.PI);
+            }
+            set
+            {
+                coefficient_b = value * 2 * Math.PI;
+            }
+        }
+        public double VerticalShift
+        {
+            get
+            {
+                return coefficient_d;
+            }
+            set => coefficient_d = value;
+        }
+
+        private double GetYCoordinate(double x_coordinate)
+        {
+            return (coefficient_a * Math.Cos(coefficient_b * (x_coordinate + coefficient_c))) + coefficient_d;
+        }
+        public List<MyPoint> GetSamples()
+        {
+            //we need 1/samplerate, starting at time = 0
+            //maybe change the graph start point eventually
+            List<MyPoint> samples = new List<MyPoint>();
+            for(int i = 0; i < SampleCount; i++)
+            {
+                double x = i * (1 / SampleRate);
+                samples.Add(new MyPoint(x,GetYCoordinate(x)));
+            }
+            return samples;
+        }
+    }
+}
---
+++ ImplantedDeviceService/ImplantedDeviceService/Services/MyPoint.cs
@@ -0,0 +1,14 @@
+﻿namespace ImplantedDeviceService.Services
+{
+    public class MyPoint
+    {
+        public MyPoint () { }
+        public MyPoint (double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
